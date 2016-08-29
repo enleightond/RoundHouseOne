12fd2237e9976d4d4af21d262cd217078ab8fbc8
enleightond <enleightond@yahoo.com>
2016-08-28 21:09:48 -0700

    cleanup pass

--- js/app.js
+++ js/app.js
@@ -1,39 +1,35 @@
-//jQuery(document).ready(function($){
+//creating the table to populate with data
+function columnHeaders(arr) {
+    var columnSet = [];
+    var headerTr$ = $('<tr/>');
 
-    //creating the table to populate with data
-    function columnHeaders(arr) {
-        var columnSet = [];
-        var headerTr$ = $('<tr/>');
-
-        for (var i = 0; i < arr.length; i++) {
-            var row = arr[i];
-            for (var key in row) {
-                if ($.inArray(key, columnSet) == -1) {
-                    columnSet.push(key);
-                    headerTr$.append($('<th/>').html(key));
-                }
+    for (var i = 0; i < arr.length; i++) {
+        var row = arr[i];
+        for (var key in row) {
+            if ($.inArray(key, columnSet) == -1) {
+                columnSet.push(key);
+                headerTr$.append($('<th/>').html(key));
             }
         }
-        $("#jsonTable").append(headerTr$);
+    }
+    $("#jsonTable").append(headerTr$);
 
-        return columnSet;
-    };
+    return columnSet;
+};
 
-    // grabbing the JSON data with jquery
-    $.getJSON("convertcsv.json", function (data) {
-        var columns = columnHeaders(data);
+// Load JSON-encoded data from the server using a GET HTTP request.
+$.getJSON("convertcsv.json", function (data) {
+    var columns = columnHeaders(data);
 
-        for (var i = 0; i < data.length; i++) {
-            var row$ = $('<tr/>');
-            for (var j = 0; j < columns.length; j++) {
-                var cellValue = data[i][columns[j]];
+    for (var i = 0; i < data.length; i++) {
+        var row$ = $('<tr/>');
+        for (var j = 0; j < columns.length; j++) {
+            var cellValue = data[i][columns[j]];
 
-                if (cellValue == null) { cellValue = ""; }
+            if (cellValue == null) { cellValue = ""; }
 
-                row$.append($('<td/>').html(cellValue));
-            }
-            $("#jsonTable").append(row$);
+            row$.append($('<td/>').html(cellValue));
         }
-    });
-
-//};
+        $("#jsonTable").append(row$);
+    }
+});
